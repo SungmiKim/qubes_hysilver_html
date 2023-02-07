0597ebb8909ea977cfc599557c5735ae86762e71
SungmiKim <62002100+SungmiKim@users.noreply.github.com>
2023-02-07 20:10:07 +0900

    230207

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-$(function () {});
+$(function () {
+  /**
+   * 모바일 메뉴
+   */
+  $("header .mo_gnb .mo_menu").click(function () {
+    $(".mo_gnb .main_menu").addClass("on");
+    $("body").addClass("fixed");
+  });
+
+  $(".mo_gnb .main_menu .links .link").click(function () {
+    $(".mo_gnb .main_menu .links .sub_menu").slideUp();
+    if ($(this).next().css("display") === "none") {
+      $(this).next().slideDown();
+    }
+  });
+
+  $(".mo_gnb .main_menu .top .btn_close").click(function () {
+    $(".mo_gnb .main_menu").removeClass("on");
+    $("body").removeClass("fixed");
+  });
+});
